2646006dc4a1b10ab3bcf19ead47dd8c9ac0d529
Marusya-Belova <m9175797050@gmail.com>
2021-11-29 09:57:41 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 13000;
+        int rubna1mile = 20;
+        int mile = ticketprice / rubna1mile;
+        System.out.println(mile);
+    }
+}
